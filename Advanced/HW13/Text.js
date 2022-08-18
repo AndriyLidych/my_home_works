789d193482daf8256aa3d3c60ff01c4e79d7ab70
Andtiy Lidych <lidichandre@gmail.com>
2022-08-18 12:15:46 +0300

    HW13 Completed

---
+++ Advanced/HW13/Text.js
@@ -0,0 +1,25 @@
+async  function getRandomChinese(length) {
+   let chineseStr = [];
+   const getSign = () => new Promise((resolved) => {
+      return setTimeout(() => {
+         const sign = Date.now().toString().slice(-5);
+         let charCode = String.fromCharCode(sign);
+         resolved(charCode);
+      }, 500);
+   });
+   
+   do {
+      await getSign().then((element) => chineseStr.push(element));
+   } while (chineseStr.length < length);
+
+   return new Promise((resolved) => {
+      setTimeout(() => {
+         resolved(console.log(chineseStr.join('')));
+      }, length * 50);
+   });
+   const x = await getRandomChinese(4);
+}
+
+console.log(x)
+
+  
